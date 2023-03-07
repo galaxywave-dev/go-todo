90d259875baae6ab60d63c07baf74d396175221b
ttwin@nete2asia.com <ttwin@nete2asia.com>
2023-03-07 17:21:01 +0630

    Add book model and controller

---
+++ apiserver/controllers/books.go
@@ -0,0 +1,96 @@
+package controllers
+
+import (
+	"net/http"
+
+	"galaxywave.com/go-todo/models"
+	"github.com/gin-gonic/gin"
+)
+
+// GET /books
+// Get all books
+type CreateBookInput struct {
+	Title  string `json:"title" binding:"required"`
+	Author string `json:"author" binding:"required"`
+}
+
+type UpdateBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+type FindBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+func FindBooks(c *gin.Context) {
+	var books []models.Book
+	models.DB.Find(&books)
+
+	c.JSON(http.StatusOK, gin.H{"data": books})
+}
+
+func CreateBook(c *gin.Context) {
+	// Validate input
+	var input CreateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Create book
+	count := models.DB.Where(&input).Find(&models.Book{}).RowsAffected
+	if count > 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record already exists!"})
+		return
+	}
+	book := models.Book{Title: input.Title, Author: input.Author}
+	models.DB.Create(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func FindBook(c *gin.Context) { // Get model if exist
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func UpdateBook(c *gin.Context) {
+	// Get model if exist
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	// Validate input
+	var input UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	models.DB.Model(&book).Updates(input)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func DeleteBook(c *gin.Context) {
+	// Get model if exist
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	models.DB.Delete(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": true})
+}
---
+++ apiserver/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"galaxywave.com/go-todo/controllers"
+	"galaxywave.com/go-todo/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	books := r.Group("/books")
+	{
+		books.GET("/", controllers.FindBooks)
+		books.POST("/", controllers.CreateBook)
+		books.GET("/:id", controllers.FindBook)
+		books.PATCH("/:id", controllers.UpdateBook)
+		books.DELETE("/:id", controllers.DeleteBook) // new
+	}
+
+	models.ConnectDatabase() // new
+
+	r.Run(":8088")
+}
